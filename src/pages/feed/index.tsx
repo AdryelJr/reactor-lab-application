74755f3591309c01ae0f229df0ba67f44305f7a3
Adryel <adryeljuniods@gmail.com>
2024-01-10 17:20:14 -0300

    up code

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Header } from "../../componentes/Header";
 
+import './style.scss'
+
 export function Feed() {
   const [isSearchFocused, setIsSearchFocused] = useState(false);
 
@@ -13,15 +15,32 @@ export function Feed() {
   };
 
   return (
-    <div>
+    <div className="feed">
       <section>
         <Header
           onSearchFocus={handleSearchFocus}
           onSearchBlur={handleSearchBlur}
         />
       </section>
+
       <section className={`body ${isSearchFocused ? 'dark-background' : ''}`}>
-        <h1>Hello world!!!!</h1>
+        <div className="feed-left">
+          feed-left
+        </div>
+
+        <div className="feed-center">
+          <div className="input">
+            input
+          </div>
+
+          <main className="feed-content">
+            main-content
+          </main>
+        </div>
+
+        <div className="feed-right">
+          feed-right
+        </div>
       </section>
     </div>
   );
