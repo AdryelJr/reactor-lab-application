87e516ab237e7103e16d5e5c486ec25667ca3cf2
Adryel <adryeljuniods@gmail.com>
2024-01-26 12:52:56 -0300

    up

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,5 +1,5 @@
 import { Header } from '../../componentes/Header';
-import { ProfileImg } from '../../componentes/ProfileImg';
+// import { ProfileImg } from '../../componentes/ProfileImg';
 import './style.scss'
 
 export function ProfilePage() {
@@ -8,15 +8,22 @@ export function ProfilePage() {
       <Header />
 
       <div className='content-profilePage'>
-        <div className='div-profile'>
-          <div className='banner'>
-            <ProfileImg />
-          </div>
+        <div className='img-profile'>
+          {/* FOTO DE PERFIL */}
+        </div>
+        <div className='cima-profile'>
+          {/* CADA DO PERFIL */}
+          <img src="https://i.pinimg.com/564x/63/4f/85/634f85c482ec7becbfea699b21a8100e.jpg" alt="capa do perfil" />
+        </div>
 
-          <div className='profile-content'>
-            <h1>name</h1>
-            <p>email</p>
-            <p>something else from the profile</p>
+        <div className='baixo-profile'>
+          {/* INFORMAÇÕES DO PERFIL */}
+          <div className='div-span-information'>
+            <span>0 <br /> Seguidores</span>
+            <span>0 <br /> Seguindo</span>
+          </div>
+          <div className='edit-profile'>
+            <button>Editar conta</button>
           </div>
         </div>
       </div>
